93d7560faabaf3d454773716e9d39eeef57f7902
Malaika Poluscar <polusmal001@warren.k12.in.us>
2025-02-20 13:46:42 -0500

    cx

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        var reverseGrade = new GradeComparator().reversed();
+        // TODO: create your age Comparator here call it age here.
+
+        // TODO: make your list of students.
+        Student.add(new Student("Alice", 10, 15 ));
+
+        // TODO: sort then using reverseGrade.andThenComparing(age)
+        students.sort(reverseGrade.thenComparing(age));
+
+        // TODO: print out the list.
+
+
+    }
+}
